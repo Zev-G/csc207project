2e7f9a8b42c82d5b2bd48e41b34ede8a0e47f668
Zev G <godfreyzev@gmail.com>
2024-10-31 10:48:43 -0400

    removed un needed code

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,7 +1,5 @@
 package app;
 
-import javax.swing.JFrame;
-
 /**
  * The Main class of our application.
  */
@@ -11,18 +9,6 @@ public class Main {
      * @param args unused arguments
      */
     public static void main(String[] args) {
-        final AppBuilder appBuilder = new AppBuilder();
-        // TODO: add the Logout Use Case to the app using the appBuilder
-        final JFrame application = appBuilder
-                                            .addLoginView()
-                                            .addSignupView()
-                                            .addLoggedInView()
-                                            .addSignupUseCase()
-                                            .addLoginUseCase()
-                                            .addChangePasswordUseCase()
-                                            .build();
 
-        application.pack();
-        application.setVisible(true);
     }
 }
