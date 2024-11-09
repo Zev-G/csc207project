904b0e1f62ddffbeeb1da74c1fbdeb52e1ffeb1b
KristopherZ <84158982+KristopherZ@users.noreply.github.com>
2024-11-09 13:20:21 -0500

    finished a mock data access class

--- src/main/java/game/DataAccessMock.java
+++ src/main/java/game/DataAccessMock.java
@@ -11,7 +11,7 @@ public class DataAccessMock implements LocationDataAccess {
         ImageIcon photo1 = new ImageIcon(ClassLoader.getSystemResource("photos/sample1.jpg"));
         PhotoLocation photoLocation1 = new PhotoLocation(photo1, new double[]{43.66742755781882, -79.39177102147445});
         ImageIcon photo2 = new ImageIcon(ClassLoader.getSystemResource("photos/sample2.jpeg"));
-        PhotoLocation photoLocation2 = new PhotoLocation(photo1, new double[]{43.65984277958618, -79.39718377820866});
+        PhotoLocation photoLocation2 = new PhotoLocation(photo2, new double[]{43.65984277958618, -79.39718377820866});
         locations.add(photoLocation1);
         locations.add(photoLocation2);
     }
