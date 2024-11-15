309366ade618fcaa1d275da4dcafaeb7d8bf1f89
Zev G <godfreyzev@gmail.com>
2024-11-15 13:04:54 -0500

    Added text to main panel and laid out the text

--- src/main/java/view/MainPanel.java
+++ src/main/java/view/MainPanel.java
@@ -1,19 +1,28 @@
 package view;
 
 import view.components.DLabel;
+import view.components.DPanel;
 import view.components.HorizontalPanel;
 import view.components.SerifLabel;
 
 import javax.swing.*;
 
-public class MainPanel extends JPanel {
+public class MainPanel extends DPanel {
 
     // UI Fields
-    private final JPanel titlePanel = new HorizontalPanel();
-    private final JLabel uoftText = new SerifLabel("UofT");
+    private final DLabel uoftText = new SerifLabel("UofT");
+    private final DLabel gameText = new DLabel("Guessr");
+    private final JPanel titlePanel = new HorizontalPanel(uoftText, gameText);
 
     public MainPanel() {
+        // Configure components
+        uoftText.setFontSize(ViewConstants.TEXT_LL);
+        gameText.setFontSize(ViewConstants.TEXT_LL);
 
+        setMargin(ViewConstants.MARGIN_M);
+
+        // Add components
+        add(titlePanel);
     }
 
 }
