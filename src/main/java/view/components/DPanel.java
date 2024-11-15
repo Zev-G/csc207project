43d551d999f9ddc1f0e2019d3c2d0b736570578a
Zev G <godfreyzev@gmail.com>
2024-11-15 13:03:30 -0500

    Created default panel class with setMargin feature

---
+++ src/main/java/view/components/DPanel.java
@@ -0,0 +1,42 @@
+package view.components;
+
+import view.ViewConstants;
+
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import java.awt.*;
+
+public class DPanel extends JPanel {
+
+    public DPanel(LayoutManager layout, boolean isDoubleBuffered) {
+        super(layout, isDoubleBuffered);
+        init();
+    }
+
+    public DPanel(LayoutManager layout) {
+        super(layout);
+        init();
+    }
+
+    public DPanel(boolean isDoubleBuffered) {
+        super(isDoubleBuffered);
+        init();
+    }
+
+    public DPanel() {
+        init();
+    }
+
+    private void init() {
+
+    }
+
+    /**
+     * Sets the margin
+     * @param margin the margin
+     */
+    public void setMargin(int margin) {
+        setBorder(new EmptyBorder(margin, margin, margin, margin));
+    }
+
+}
