68586c25240084ad4fc115a64e06e9e55e1636a2
Zev G <godfreyzev@gmail.com>
2024-11-15 13:34:50 -0500

    Created HTMLTextBuilder

--- src/main/java/view/MainPanel.java
+++ src/main/java/view/MainPanel.java
@@ -10,7 +10,12 @@ public class MainPanel extends DPanel {
     // UI Fields
     private final DLabel uoftText = new SerifLabel("UofT");
     private final DLabel gameText = new DLabel("Guessr");
-    private final DLabel subtitleText = new DLabel("<html><div style='text-align: center;'>Test your knowledge of UofT by guessing<br>where these photos were taken</div></html>");
+    private final DLabel subtitleText = new DLabel(
+            new HTMLTextBuilder()
+                    .addText("Test your knowledge of UofT by guessing")
+                    .addLineBreak()
+                    .addText("where these photos were taken")
+                    .center().build());
     private final DPanel titlePanel = new HorizontalPanel(uoftText, gameText);
     private final DPanel subtitlePanel = new HorizontalPanel(subtitleText);
     private final VerticalPanel layout = new VerticalPanel(titlePanel, subtitlePanel);
