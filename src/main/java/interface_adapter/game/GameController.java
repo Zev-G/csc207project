edf9777ee4d199def6e74bb0ca1dbb9a1334def3
KristopherZ <84158982+KristopherZ@users.noreply.github.com>
2024-11-24 17:27:03 -0500

    Changes by terre

--- src/main/java/interface_adapter/game/GameController.java
+++ src/main/java/interface_adapter/game/GameController.java
@@ -1,4 +1,21 @@
 package interface_adapter.game;
 
+import use_case.game.GameInputBoundary;
+import use_case.game.GameInputData;
+
 public class GameController {
+
+    final GameInputBoundary gameInteractor;
+
+    public GameController(GameInputBoundary gameInteractor) {
+        this.gameInteractor = gameInteractor;
+    }
+
+    public void handleGuess(int photoID, double[] target, double[] chosen) {
+        gameInteractor.handleGuess(new GameInputData(photoID, target, chosen));
+    }
+
+    public void init(){
+        this.gameInteractor.init();
+    }
 }
