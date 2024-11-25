2c6b3277210dababc759d1712173f8dfb682dad0
Zev G <godfreyzev@gmail.com>
2024-11-25 16:01:09 -0500

    Created test for account confirm interactor

--- src/main/java/use_case/accountconfirm/AccountConfirmInteractor.java
+++ src/main/java/use_case/accountconfirm/AccountConfirmInteractor.java
@@ -1,18 +1,27 @@
 package use_case.accountconfirm;
 
+import data_access.UserDataAccess;
 import use_case.account.AccountInputData;
 
 public class AccountConfirmInteractor implements AccountConfirmInputBoundary {
 
     private final AccountConfirmOutputBoundary presenter;
+    private final UserDataAccess userDataAccess;
 
-    public AccountConfirmInteractor(AccountConfirmOutputBoundary presenter) {
+    public AccountConfirmInteractor(UserDataAccess userDataAccess, AccountConfirmOutputBoundary presenter) {
         this.presenter = presenter;
+        this.userDataAccess = userDataAccess;
     }
 
     @Override
     public void pressed(AccountInputData inputData) {
         // Do data base stuff
+        boolean success1 = userDataAccess.changeEmail(inputData.getUserId(), inputData.getEmail());
+        boolean success2 = userDataAccess.changeUsername(inputData.getUserId(), inputData.getUsername());
+        if (!success1 || !success2) {
+            presenter.handleFail();
+            return;
+        }
         presenter.handleSuccess();
     }
 
