74736182e9cc992346d1f3916af1ebe7bfa62edd
Zev G <godfreyzev@gmail.com>
2024-11-25 15:59:49 -0500

    Created user data access interface

---
+++ src/main/java/data_access/UserDataAccess.java
@@ -0,0 +1,12 @@
+package data_access;
+
+import entity.CommonUser;
+import entity.User;
+
+public interface UserDataAccess {
+
+    boolean changeUsername(int uid, String username);
+    boolean changeEmail(int uid, String email);
+    User getUser(int uid);
+
+}
