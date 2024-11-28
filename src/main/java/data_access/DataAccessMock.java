c3bcebce0abae54d28867bb8f67d6ccd56139c86
Aksshatt <114461663+aksshatt7@users.noreply.github.com>
2024-11-28 00:55:11 -0500

    Additonal image functionality in mock data

--- src/main/java/data_access/DataAccessMock.java
+++ src/main/java/data_access/DataAccessMock.java
@@ -54,16 +54,18 @@ public class DataAccessMock implements LocationDataAccess, UserDataAccess {
                 JSONObject json = new JSONObject(responseBody);
                 JSONArray imagesArray = json.getJSONArray("data");
 
-                // Ensure we have at least three images for the predefined PhotoLocation objects
-                if (imagesArray.length() >= 3) {
-                    // Use the first three images from Imgur for the predefined PhotoLocation objects
+                // Ensure we have at least four images for the predefined PhotoLocation objects
+                if (imagesArray.length() >= 4) {
+                    // Use the first four images from Imgur for the predefined PhotoLocation objects
                     JSONObject image1 = imagesArray.getJSONObject(0);
                     JSONObject image2 = imagesArray.getJSONObject(1);
                     JSONObject image3 = imagesArray.getJSONObject(2);
+                    JSONObject image4 = imagesArray.getJSONObject(3);
 
                     String imageUrl1 = image1.getString("link");
                     String imageUrl2 = image2.getString("link");
                     String imageUrl3 = image3.getString("link");
+                    String imageUrl4 = image4.getString("link");
 
                     ImageIcon photo1 = new ImageIcon(new URL(imageUrl1));
                     PhotoLocation photoLocation1 = new PhotoLocation(photo1, new double[]{43.66742755781882, -79.39177102147445}, 1);
@@ -74,9 +76,13 @@ public class DataAccessMock implements LocationDataAccess, UserDataAccess {
                     ImageIcon photo3 = new ImageIcon(new URL(imageUrl3));
                     PhotoLocation photoLocation3 = new PhotoLocation(photo3, new double[]{43.662891, -79.395656}, 3);
 
+                    ImageIcon photo4 = new ImageIcon(new URL(imageUrl4));
+                    PhotoLocation photoLocation4 = new PhotoLocation(photo4, new double[]{43.665123, -79.400456}, 4);
+
                     locations.add(photoLocation1);
                     locations.add(photoLocation2);
                     locations.add(photoLocation3);
+                    locations.add(photoLocation4);
 
                     System.out.println("Predefined PhotoLocation objects updated with Imgur images.");
                 } else {
