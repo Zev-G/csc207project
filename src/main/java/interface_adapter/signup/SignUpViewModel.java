6d46a3cd27e12472101f970d6c139165a0245599
r11creates <rithvik11sunil@gmail.com>
2024-11-27 22:26:10 -0500

    modifying sign up view model

--- src/main/java/interface_adapter/signup/SignUpViewModel.java
+++ src/main/java/interface_adapter/signup/SignUpViewModel.java
@@ -1,16 +1,18 @@
 package interface_adapter.signup;
 
-import data_access.FirebaseUserDataAccess;
 import interface_adapter.ViewModel;
+import data_access.FirebaseUserDataAccess;
+import java.util.concurrent.CompletableFuture;
 
+/**
+ * ViewModel for handling sign-up logic.
+ */
 public class SignUpViewModel extends ViewModel<SignUpState> {
-
     private final FirebaseUserDataAccess userDataAccess;
 
     public SignUpViewModel(FirebaseUserDataAccess userDataAccess) {
         super("signup");
         this.userDataAccess = userDataAccess;
-        setState(SignUpState.DUMMY_STATE);
+        setState(SignUpState.initial());
     }
-
 }
