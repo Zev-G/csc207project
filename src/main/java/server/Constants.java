a391620e1c8c73413f122bb006a430984b035932
KristopherZ <84158982+KristopherZ@users.noreply.github.com>
2024-11-29 16:18:01 -0500

    java docs added

--- src/main/java/server/Constants.java
+++ src/main/java/server/Constants.java
@@ -4,9 +4,29 @@ package server;
  * Constants for the server.
  */
 public class Constants {
+
+    /**
+     * Convert second to millisecond.
+     */
     public static final int SECOND2MILLISECOND = 1000;
+
+    /**
+     * Convert minute to second.
+     */
     public static final int MINUTE2SECOND = 60;
+
+    /**
+     * Timeout time,
+     */
     public static final int TIME_OUT_TIME = 120;
+
+    /**
+     * Game time per round.
+     */
     public static final int GAME_TIME = 70;
+
+    /**
+     * Port for the server to mount.
+     */
     public static final int PORT = 5555;
 }
