b4a692dcbe24eafc9d8a9f5c595afd75b1dda073
r11creates <rithvik11sunil@gmail.com>
2024-11-28 20:22:30 -0500

    method for sign up presenter

--- src/main/java/interface_adapter/signup/SignUpPresenter.java
+++ src/main/java/interface_adapter/signup/SignUpPresenter.java
@@ -10,4 +10,10 @@ public class SignUpPresenter implements SignUpOutputBoundary {
     public SignUpPresenter(SignUpViewModel viewModel) {
         this.viewModel = viewModel;
     }
+
+    @Override
+    public void present(SignUpOutputData outputData) {
+        SignUpState state = new SignUpState(outputData.isSuccess(), outputData.getErrorMessage());
+        viewModel.setState(state);
+    }
 }
