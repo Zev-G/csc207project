326a11b7de952ffccf76e710b5371aeaafa716e0
r11creates <rithvik11sunil@gmail.com>
2024-11-28 19:03:46 -0500

    finishing the interactor up

--- src/main/java/use_case/signup/SignUpInteractor.java
+++ src/main/java/use_case/signup/SignUpInteractor.java
@@ -2,7 +2,7 @@ package use_case.signup;
 
 import data_access.SignUpDataAccess;
 
-public class SignUpInteractor implements SignUpDataAccess {
+public class SignUpInteractor implements SignUpInputBoundary {
 
     private final SignUpDataAccess dataAccess;
     private final SignUpOutputBoundary outputBoundary;
@@ -10,5 +10,21 @@ public class SignUpInteractor implements SignUpDataAccess {
     public SignUpInteractor(SignUpDataAccess dataAccess, SignUpOutputBoundary outputBoundary) {
         this.dataAccess = dataAccess;
         this.outputBoundary = outputBoundary;
+    }
+
+    @Override
+    public void execute(SignUpInputData inputData) {
+        try {
+            // create an user via data access
+            dataAccess.createUser(inputData);
+
+            // prepare success output
+            outputBoundary.present(new SignUpOutputData(true, null));
+        }
+        catch (Exception e) {
+
+            // prepare failure output
+            outputBoundary.present(new SignUpOutputData(false, e.getMessage()));
+        }
     }
 }
