27d27577285d2462075e1db066a2e20524fbc082
r11creates <rithvik11sunil@gmail.com>
2024-11-28 19:05:30 -0500

    initializing varibales for sign up input data

---
+++ src/main/java/use_case/signup/SignUpInputData.java
@@ -0,0 +1,14 @@
+package use_case.signup;
+
+public class SignUpInputData {
+
+    private final String username;
+    private final String email;
+    private final String password;
+
+    public SignUpInputData(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+}
