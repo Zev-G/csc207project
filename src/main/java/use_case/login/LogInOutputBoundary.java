2ddae86e7694d357ebc811116502e928ad5739d0
r11creates <rithvik11sunil@gmail.com>
2024-12-01 10:27:28 -0500

    log in output boundary

---
+++ src/main/java/use_case/login/LogInOutputBoundary.java
@@ -0,0 +1,8 @@
+package use_case.login;
+
+/**
+ * The output boundary interface for presenting log-in results.
+ */
+public interface LogInOutputBoundary {
+    void present(LogInOutputData outputData);
+}
