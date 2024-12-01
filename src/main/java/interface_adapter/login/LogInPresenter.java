f783cb1c5f25cf65252e8fd0c7e257a428af2977
r11creates <rithvik11sunil@gmail.com>
2024-12-01 10:42:30 -0500

    log in presenter

---
+++ src/main/java/interface_adapter/login/LogInPresenter.java
@@ -0,0 +1,21 @@
+package interface_adapter.login;
+
+import use_case.login.LogInOutputBoundary;
+import use_case.login.LogInOutputData;
+
+/**
+ * The presenter for the log-in process.
+ */
+public class LogInPresenter implements LogInOutputBoundary {
+    private final LogInViewModel viewModel;
+
+    public LogInPresenter(LogInViewModel viewModel) {
+        this.viewModel = viewModel;
+    }
+
+    @Override
+    public void present(LogInOutputData outputData) {
+        LogInState state = new LogInState(outputData.isSuccess(), outputData.getMessage());
+        viewModel.setState(state);
+    }
+}
