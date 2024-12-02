5768ad3cb7b8ee4929c3e7efd24e08d7775963b7
Zev G <godfreyzev@gmail.com>
2024-12-01 23:16:41 -0500

    Created system to handle errors

---
+++ src/main/java/interface_adapter/ErrorHandlingModel.java
@@ -0,0 +1,12 @@
+package interface_adapter;
+
+public class ErrorHandlingModel extends ViewModel<String> {
+    /**
+     * Constructs a ViewModel with a given view name.
+     *
+     * @param viewName the name of the view managed by this ViewModel
+     */
+    public ErrorHandlingModel(String viewName) {
+        super(viewName);
+    }
+}
