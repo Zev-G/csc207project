77715ced0d8cc9058a0571f4bb06a54bf43ab328
Zev G <godfreyzev@gmail.com>
2024-12-01 23:18:07 -0500

    renamed to be more consistent

--- src/main/java/interface_adapter/ErrorHandlingViewModel.java
+++ src/main/java/interface_adapter/ErrorHandlingViewModel.java
@@ -1,12 +1,12 @@
 package interface_adapter;
 
-public class ErrorHandlingModel extends ViewModel<String> {
+public class ErrorHandlingViewModel extends ViewModel<String> {
     /**
      * Constructs a ViewModel with a given view name.
      *
      * @param viewName the name of the view managed by this ViewModel
      */
-    public ErrorHandlingModel(String viewName) {
+    public ErrorHandlingViewModel(String viewName) {
         super(viewName);
     }
 }
