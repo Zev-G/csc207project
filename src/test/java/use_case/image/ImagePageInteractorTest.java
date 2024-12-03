96d06f2d0c21201ee6ed5a9f924a40577324aac2
Aksshatt <114461663+aksshatt7@users.noreply.github.com>
2024-12-03 08:56:21 -0500

    Test for image page interactor with full coverage

---
+++ src/test/java/use_case/image/ImagePageInteractorTest.java
@@ -0,0 +1,19 @@
+package use_case.image;
+
+import data_access.ImageUploadDataAccess;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.io.File;
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+/**
+ * Unit tests for the {@code ImagePageInteractor} class.
+ */
+class ImagePageInteractorTest {
+
+}
